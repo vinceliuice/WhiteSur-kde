3d80dc97fd3008c4648937f8d7e39014f874a7eb
vinceliuice <vinceliuice@hotmail.com>
2024-09-10 12:32:48 +0800

    update

--- install.sh
+++ install.sh
@@ -26,6 +26,8 @@ LATTE_DIR="$HOME/.config/latte"
 
 COLOR_VARIANTS=('' '-dark')
 PCOLOR_VARIANTS=('' '-alt' '-dark')
+WINDOW_VARIANTS=('' '-opaque' '-sharp')
+SCALE_VARIANTS=('' '_x1.25' '_x1.5' '_x1.75' '_x2.0')
 
 usage() {
   cat << EOF
@@ -34,98 +36,82 @@ Usage: $0 [OPTION]...
 OPTIONS:
   -n, --name NAME         Specify theme name (Default: $THEME_NAME)
   -c, --color VARIANT     Specify color variant(s) [light|alt|dark] (Default: All variants)s)
-  --sharp VARIANT         Specify sharp theme variant
-  --opaque VARIANT        Specify opaque theme variant
+  -w, --window VARIANT    Specify window variant(s) [default|opaque|sharp] (Default: round blur version)
   -h, --help              Show help
 EOF
 }
 
-[[ ! -d ${AURORAE_DIR} ]] && mkdir -p ${AURORAE_DIR}
-[[ ! -d ${SCHEMES_DIR} ]] && mkdir -p ${SCHEMES_DIR}
-[[ ! -d ${PLASMA_DIR} ]] && mkdir -p ${PLASMA_DIR}
-[[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
-[[ ! -d ${KVANTUM_DIR} ]] && mkdir -p ${KVANTUM_DIR}
-[[ ! -d ${WALLPAPER_DIR} ]] && mkdir -p ${WALLPAPER_DIR}
+[[ ! -d "${AURORAE_DIR}" ]] && mkdir -p "${AURORAE_DIR}"
+[[ ! -d "${SCHEMES_DIR}" ]] && mkdir -p "${SCHEMES_DIR}"
+[[ ! -d "${PLASMA_DIR}" ]] && mkdir -p "${PLASMA_DIR}"
+[[ ! -d "${LOOKFEEL_DIR}" ]] && mkdir -p "${LOOKFEEL_DIR}"
+[[ ! -d "${KVANTUM_DIR}" ]] && mkdir -p "${KVANTUM_DIR}"
+[[ ! -d "${WALLPAPER_DIR}" ]] && mkdir -p "${WALLPAPER_DIR}"
 
-cp -rf "${SRC_DIR}"/configs/Xresources "$HOME"/.Xresources
+# cp -rf "${SRC_DIR}"/configs/Xresources "$HOME"/.Xresources
 
 install() {
-  local name=${1}
-  local color=${2}
+  local name="${1}"
+  local color="${2}"
 
-  [[ ${color} == '-dark' ]] && local ELSE_COLOR='Dark'
-  [[ ${color} == '-light' ]] && local ELSE_COLOR='Light'
+  [[ "${color}" == '-dark' ]] && local ELSE_COLOR='Dark'
+  [[ "${color}" == '-light' ]] && local ELSE_COLOR='Light'
 
-  [[ -d ${AURORAE_DIR}/${name}${color} ]] && rm -rf ${AURORAE_DIR}/${name}${color}*
-  [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rf ${KVANTUM_DIR}/${name}
-  [[ -d ${WALLPAPER_DIR}/${name} ]] && rm -rf ${WALLPAPER_DIR}/${name}*
-  [[ -f ${LATTE_DIR}/${name}.layout.latte ]] && rm -rf ${LATTE_DIR}/${name}*.layout.latte
+  [[ -d "${KVANTUM_DIR}/${name}" ]] && rm -rf "${KVANTUM_DIR}/${name}"
+  [[ -d "${WALLPAPER_DIR}/${name}" ]] && rm -rf "${WALLPAPER_DIR}/${name}"*
+  [[ -f "${LATTE_DIR}/${name}.layout.latte" ]] && rm -rf "${LATTE_DIR}/${name}*.layout.latte"
 
-  if [[ "${sharp}" == "true" ]]; then
-    cp -r ${SRC_DIR}/aurorae/sharp/${name}${color}*                                  ${AURORAE_DIR}
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-sharp
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-sharp_x1.25
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-sharp_x1.5
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-sharp_x1.75
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-sharp_x2.0
-  elif [[ "${opaque}" == "true" ]]; then
-    cp -r ${SRC_DIR}/aurorae/opaque/${name}${color}*                                 ${AURORAE_DIR}
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-opaque
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-opaque_x1.25
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-opaque_x1.5
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-opaque_x1.75
-    cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                             ${AURORAE_DIR}/${name}${color}-opaque_x2.0
-  fi
-
-  cp -r ${SRC_DIR}/aurorae/normal/${name}${color}*                                   ${AURORAE_DIR}
-  cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                               ${AURORAE_DIR}/${name}${color}
-  cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                               ${AURORAE_DIR}/${name}${color}_x1.25
-  cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                               ${AURORAE_DIR}/${name}${color}_x1.5
-  cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                               ${AURORAE_DIR}/${name}${color}_x1.75
-  cp -r ${SRC_DIR}/aurorae/common/assets${color}/*.svg                               ${AURORAE_DIR}/${name}${color}_x2.0
-
-  cp -r ${SRC_DIR}/Kvantum/${name}                                                   ${KVANTUM_DIR}
+  cp -r "${SRC_DIR}/Kvantum/${name}"                                                   "${KVANTUM_DIR}"
 
   if [[ "${opaque}" == "true" ]]; then
-    cp -r ${SRC_DIR}/Kvantum/${name}-opaque                                          ${KVANTUM_DIR}
+    cp -r "${SRC_DIR}/Kvantum/${name}-opaque"                                          "${KVANTUM_DIR}"
   fi
 
-  cp -r ${SRC_DIR}/wallpaper/${name}*                                                ${WALLPAPER_DIR}
-  [[ -d ${LATTE_DIR} ]] && cp -r ${SRC_DIR}/latte-dock/*                             ${LATTE_DIR}
+  cp -r "${SRC_DIR}/wallpaper/${name}"*                                                "${WALLPAPER_DIR}"
+  [[ -d "${LATTE_DIR}" ]] && cp -r "${SRC_DIR}/latte-dock/"*                           "${LATTE_DIR}"
 }
 
 install_plasma() {
-  local name=${1}
-  local pcolor=${2}
+  local name="${1}"
+  local pcolor="${2}"
 
-  [[ ${pcolor} == '-dark' ]] && local ELSE_COLOR='Dark'
-  [[ ${pcolor} == '-light' ]] && local ELSE_COLOR='Light'
-  [[ ${pcolor} == '-alt' ]] && local ELSE_COLOR='Alt'
+  [[ "${pcolor}" == '-dark' ]] && local ELSE_COLOR='Dark'
+  [[ "${pcolor}" == '-light' ]] && local ELSE_COLOR='Light'
+  [[ "${pcolor}" == '-alt' ]] && local ELSE_COLOR='Alt'
 
-  [[ -d ${PLASMA_DIR}/${name}${pcolor} ]] && rm -rf ${PLASMA_DIR}/${name}${pcolor}
-  [[ -f ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors
-  [[ -d ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${pcolor} ]] && rm -rf ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${pcolor}
+  [[ -d "${PLASMA_DIR}/${name}${pcolor}" ]] && rm -rf "${PLASMA_DIR}/${name}${pcolor}"
+  [[ -f "${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors" ]] && rm -rf "${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors"
+  [[ -d "${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${pcolor}" ]] && rm -rf "${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${pcolor}"
 
-  cp -r ${SRC_DIR}/color-schemes/*                                                   ${SCHEMES_DIR}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}${pcolor}                              ${PLASMA_DIR}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/icons                                         ${PLASMA_DIR}/${name}${pcolor}
-  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${pcolor}      ${LOOKFEEL_DIR}
+  cp -r "${SRC_DIR}/color-schemes/"*                                                   "${SCHEMES_DIR}"
+  cp -r "${SRC_DIR}/plasma/desktoptheme/${name}${pcolor}"                              "${PLASMA_DIR}"
+  cp -r "${SRC_DIR}/plasma/desktoptheme/icons"                                         "${PLASMA_DIR}/${name}${pcolor}"
+  cp -r "${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${pcolor}"      "${LOOKFEEL_DIR}"
+}
+
+install_aurorae() {
+  local name="${1}"
+  local color="${2}"
+  local window="${3}"
+  local scale="${4}"
+
+  local AURORAE_THEME="${AURORAE_DIR}/${name}${color}${window}${scale}"
+
+  [[ -d "${AURORAE_THEME}" ]] && rm -rf "${AURORAE_THEME}"
+
+  cp -r "${SRC_DIR}/aurorae/main${window}/${name}${color}${window}${scale}"          "${AURORAE_THEME}"
+  cp -r "${SRC_DIR}/aurorae/common/assets${color}/"*.svg                             "${AURORAE_THEME}"
+
+  cp -r "${SRC_DIR}/aurorae/"{metadata.desktop,metadata.json}                        "${AURORAE_THEME}"
+  cp -r "${SRC_DIR}/aurorae/main${window}/${name}${color}${window}rc"                "${AURORAE_THEME}/${name}${color}${window}${scale}rc"
+
+  sed -i "s/WhiteSur/${name}${color}${window}${scale}/g" "${AURORAE_THEME}/metadata.desktop" "${AURORAE_THEME}/metadata.json"
 }
 
 while [[ "$#" -gt 0 ]]; do
   case "${1:-}" in
     -n|--name)
       name="${1}"
-      shift
-      ;;
-    --sharp)
-      sharp='true'
-      echo -e "Install sharp Aurorae version."
-      shift
-      ;;
-    --opaque)
-      opaque='true'
-      echo -e "Install opaque theme version."
       shift
       ;;
     -c|--color)
@@ -145,6 +131,37 @@ while [[ "$#" -gt 0 ]]; do
           dark)
             colors+=("${COLOR_VARIANTS[1]}")
             pcolors+=("${PCOLOR_VARIANTS[2]}")
+            shift
+            ;;
+          -*)
+            break
+            ;;
+          *)
+            echo -e "ERROR: Unrecognized color variant '$1'."
+            echo -e "Try '$0 --help' for more information."
+            exit 1
+            ;;
+        esac
+      done
+      ;;
+    -w|--window)
+      shift
+      for window in "$@"; do
+        case "$window" in
+          default)
+            windows+=("${WINDOW_VARIANTS[0]}")
+            shift
+            ;;
+          opaque)
+            windows+=("${WINDOW_VARIANTS[1]}")
+            opaque='true'
+            echo -e "Install opaque theme version."
+            shift
+            ;;
+          sharp)
+            windows+=("${WINDOW_VARIANTS[2]}")
+            sharp='true'
+            echo -e "Install sharp theme version."
             shift
             ;;
           -*)
@@ -178,6 +195,14 @@ done
 
 for pcolor in "${pcolors[@]:-${PCOLOR_VARIANTS[@]}}"; do
   install_plasma "${name:-${THEME_NAME}}" "${pcolor}"
+done
+
+for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+  for window in "${windows[@]:-${WINDOW_VARIANTS[0]}}"; do
+    for scale in "${scales[@]:-${SCALE_VARIANTS[@]}}"; do
+      install_aurorae "${name:-${THEME_NAME}}" "${color}" "${window}" "${scale}"
+    done
+  done
 done
 
 echo -e "Install finished..."
